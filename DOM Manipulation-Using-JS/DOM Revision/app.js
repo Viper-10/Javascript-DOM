67a6753efa689e0e6bd5a37db1d5d6f19e85b94b
Viper-10 <priyadharshanbeepee@gmail.com>
2022-03-11 22:48:56 +0530

    add - DOM Revision folder add - Counter, Game guesser game

---
+++ DOM Manipulation-Using-JS/DOM Revision/app.js
@@ -0,0 +1,109 @@
+const plusButton = document.getElementById("plus-button");
+const minusButton = document.getElementById("minus-button");
+const countSpan = document.getElementById("count-value");
+const guessInput = document.querySelector("input");
+const progressMessage = document.getElementById("progress-message");
+const startButton = document.getElementById("start-game-button");
+
+let guessesLeft;
+let guessNumber;
+let countValue = 0;
+
+function displayFriendsInConsole() {
+  // using nodelist I can directly iterate
+  let friends = document.querySelectorAll(".friends-list li");
+
+  console.log("Friends displayed");
+  friends.forEach((friend) => {
+    console.log(friend.textContent);
+  });
+
+  console.log("friends displayed 2");
+  // using html collection I've to convert to array
+  // to use foreach.
+  friends = document.getElementsByClassName("friend");
+
+  Array.from(friends).forEach((friend) => {
+    console.log(friend.textContent);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateCounterValue(0);
+  guessInput.disabled = true;
+});
+
+const setProgressMessage = (message, color = "blue") => {
+  progressMessage.textContent = message;
+  progressMessage.style.color = color;
+};
+const updateCounterValue = (count) => {
+  countValue = count;
+  countSpan.textContent = count;
+};
+const startgame = () => {
+  guessNumber = Math.floor(Math.random() * 5);
+  guessesLeft = 3;
+  guessInput.value = "";
+  setProgressMessage(`Number of guesses left : ${guessesLeft}`);
+  guessInput.disabled = false;
+  startButton.disabled = true;
+};
+const endgame = (result) => {
+  if (result === "won") {
+    setProgressMessage("You won!!!", "green");
+    startButton.textContent = "Play Again";
+  } else {
+    setProgressMessage("You lost, try again", "red");
+    startButton.textContent = "Retry";
+  }
+  startButton.disabled = false;
+  guessInput.disabled = true;
+};
+const guessInputChange = (event) => {
+  let value = event.target.value;
+  if (value === "") {
+    setProgressMessage(`Number of guesses left : ${guessesLeft}`);
+    return;
+  }
+  if (isNaN(value) || value > 5) {
+    setProgressMessage(`Enter only numbers between 0 and 5`);
+
+    return;
+  }
+  value = parseInt(value);
+
+  if (value === guessNumber) {
+    endgame("won");
+  } else {
+    guessesLeft -= 1;
+    setProgressMessage(
+      `Incorrect guess! You have ${guessesLeft} guesses left`,
+      "red"
+    );
+    event.target.value = "";
+  }
+
+  if (guessesLeft === 0) {
+    endgame("lose");
+  }
+};
+
+const clickEventHandler = (event) => {
+  if (event.target.id === "plus-button") {
+    updateCounterValue(countValue + 1);
+  } else if (event.target.id === "minus-button") {
+    updateCounterValue(countValue - 1);
+  } else if (event.target.id === "reset-button") {
+    updateCounterValue(0);
+  } else if (event.target.id === "start-game-button") {
+    startgame();
+  }
+};
+function loadEventHandlers(event) {
+  document.addEventListener("click", clickEventHandler);
+  guessInput.addEventListener("keyup", guessInputChange);
+}
+
+loadEventHandlers();
+displayFriendsInConsole();
--- Number Guesser/app.js
+++ Number Guesser/app.js
@@ -1,19 +1,21 @@
-const start = 1; 
-const end = 10; 
-let numberOfGuessesLeft = 3; 
-const minNumber = document.getElementById('min-num')
-const maxNumber = document.getElementById('max-num')
-const winningNumber = parseInt(Math.floor(Math.random()* (end-start) + start));
+const start = 1;
+const end = 10;
+let numberOfGuessesLeft = 3;
+const minNumber = document.getElementById("min-num");
+const maxNumber = document.getElementById("max-num");
+const winningNumber = parseInt(
+  Math.floor(Math.random() * (end - start) + start)
+);
 
-const guessInput = document.getElementById('guess-input')
-const guessBtn = document.getElementById('guess-btn')
-const outputMessage = document.getElementById('message')
-const game = document.querySelector('#game')
+const guessInput = document.getElementById("guess-input");
+const guessBtn = document.getElementById("guess-btn");
+const outputMessage = document.getElementById("message");
+const game = document.querySelector("#game");
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  minNumber.textContent = start;  
-  maxNumber.textContent = end;  
-})
+document.addEventListener("DOMContentLoaded", () => {
+  minNumber.textContent = start;
+  maxNumber.textContent = end;
+});
 
 /* 
   Mousedown event vs click event. 
@@ -28,64 +30,68 @@ document.addEventListener('DOMContentLoaded', ()=>{
   When we have mousedown as an event, it is fired before this click event, and by that time guessbtn doesn't have play-again btn. 
 
   If we have click event on 'play-again' instead of mousedown event, due to eventbubbling, when the event bubbles to the parent, we would have already added 'play-again' class to the button in the event handler of last submit button(in gameOver function). 
-
-
 */
-game.addEventListener('mousedown', (e) =>{
-
-  if(e.target.classList.contains('play-again')){
+game.addEventListener("mousedown", (e) => {
+  if (e.target.classList.contains("play-again")) {
     window.location.reload();
   }
-})
-guessBtn.addEventListener('click', checkGuess);
+});
+guessBtn.addEventListener("click", checkGuess);
 
-function checkGuess(e){
+function checkGuess(e) {
   guess = parseInt(guessInput.value);
 
-  if(isNaN(guess) || guess < start || guess > end){
-    setMessageOutput('red', `Enter a number between ${start} and ${end}`);
+  if (isNaN(guess) || guess < start || guess > end) {
+    setMessageOutput("red", `Enter a number between ${start} and ${end}`);
 
-    return; 
+    return;
   }
 
-  if(guess === winningNumber){
-    setMessageOutput('green', `You Won!!! ${winningNumber} is the correct guess`)
-    gameOver(); 
+  if (guess === winningNumber) {
+    setMessageOutput(
+      "green",
+      `You Won!!! ${winningNumber} is the correct guess`
+    );
+    gameOver();
     return;
-  }  
-
-  else{
-    numberOfGuessesLeft--; 
-    if(numberOfGuessesLeft === 0){
-      setMessageOutput('red', `You Lost, ${winningNumber} is the correct number`);
-      gameOver(); 
-    }
-    else{
-      if(guess > winningNumber) {
-        setMessageOutput('red', `${guess} is larger than the winning number, you have ${numberOfGuessesLeft} more guesses`) 
-      }
-
-      else{
-        setMessageOutput('red', `${guess} is smaller than the winning number,you have ${numberOfGuessesLeft} more guesses`);
+  } else {
+    numberOfGuessesLeft--;
+    if (numberOfGuessesLeft === 0) {
+      setMessageOutput(
+        "red",
+        `You Lost, ${winningNumber} is the correct number`
+      );
+      gameOver();
+    } else {
+      if (guess > winningNumber) {
+        setMessageOutput(
+          "red",
+          `${guess} is larger than the winning number, you have ${numberOfGuessesLeft} more guesses`
+        );
+      } else {
+        setMessageOutput(
+          "red",
+          `${guess} is smaller than the winning number,you have ${numberOfGuessesLeft} more guesses`
+        );
       }
     }
   }
 }
 
-function setMessageOutput(color, text){
-  outputMessage.textContent = text; 
+function setMessageOutput(color, text) {
+  outputMessage.textContent = text;
   outputMessage.style.color = color;
   guessInput.style.borderColor = color;
-
 }
-function gameOver(){
-  guessInput.disabled = true;     
-  guessBtn.value = 'Play Again';
-  guessBtn.className += 'play-again'
+function gameOver() {
+  guessInput.disabled = true;
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
 
-  let playAgainText = document.createElement('p')
+  let playAgainText = document.createElement("p");
 
-  playAgainText.appendChild(document.createTextNode('Click Play again to play again'))
-  playAgainText.style.color = '#00f';
-  
+  playAgainText.appendChild(
+    document.createTextNode("Click Play again to play again")
+  );
+  playAgainText.style.color = "#00f";
 }
